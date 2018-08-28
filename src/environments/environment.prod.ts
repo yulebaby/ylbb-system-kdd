d91f0927e5295b67e46c4a4bc41fd7857f69b79a
phuhoang <phuhoang@sina.com>
2018-08-28 12:59:13 +0800

    --

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: '1.0.4',
-  // domain: 'http://keduo.beibeiyue.com/k',
-  domain: 'http://192.168.1.110:8081'
+  domain: 'http://keduo.beibeiyue.com/k',
+  // domain: 'http://192.168.1.110:8081'
 };
